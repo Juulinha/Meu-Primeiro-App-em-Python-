755ed3b27c53c9594bddfb994286c8c9588dba81
Juulinha <116567026+Juulinha@users.noreply.github.com>
2022-10-25 11:19:31 -0300

    Criei app icm
    Iniciado o desenvolvimento de entrada e saida de dados do imc.

---
+++ Olá Mundo.py
@@ -0,0 +1,12 @@
+# coletando dados de usario 
+nome = input("digite seu nome: ")
+idade = int(input("digite sua idade: "))
+altura = float(input("digite sua altura: "))
+peso = float(input("digite seu peso "))
+
+print("-" * 30)
+print("os dados coletados foram: ")
+print("Nome: ",nome)
+print("idade: ",idade, " anos")
+print("altura: ",altura)
+print("peso: ", peso, " kgs")
